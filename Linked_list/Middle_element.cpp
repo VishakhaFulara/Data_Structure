69129d3599d906866d189cb9aae3ac53bde4c382
VishakhaFulara <75986128+VishakhaFulara@users.noreply.github.com>
2021-01-21 21:31:18 +0530

    Create Middle_element.cpp

---
+++ Linked_list/Middle_element.cpp
@@ -0,0 +1,62 @@
+//Finding middle element in a linked list.
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+/* Function to get the middle of the linked list*/
+int getMiddle(Node *head);
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		cout<<getMiddle(head)<<endl;
+	}
+	return 0; 
+} 
+
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+   // Your code here
+   if(head==NULL) return -1;
+    Node *slow=head,*fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow->data;
+    
+}
