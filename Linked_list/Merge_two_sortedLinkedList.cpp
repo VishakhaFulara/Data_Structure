9de2fef86b082bee9c8fda061c9756b84de34a62
VishakhaFulara <75986128+VishakhaFulara@users.noreply.github.com>
2021-01-22 22:59:44 +0530

    Create Merge_two_sortedLinkedList.cpp

---
+++ Linked_list/Merge_two_sortedLinkedList.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+Node* sortedMerge(struct Node* a, struct Node* b);
+
+/* Function to print Nodes in a given linked list */
+void printList(struct Node *n)
+{
+    while (n!=NULL)
+    {
+        cout << n->data << " ";
+        n = n->next;
+    }
+    cout << endl;
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+        int data;
+        cin>>data;
+        struct Node *head1 = new Node(data);
+        struct Node *tail1 = head1;
+        for (int i = 1; i < n; ++i)
+        {
+            cin>>data;
+            tail1->next = new Node(data);
+            tail1 = tail1->next;
+        }
+
+        cin>>data;
+        struct Node *head2 = new Node(data);
+        struct Node *tail2 = head2;
+        for(int i=1; i<m; i++)
+        {
+            cin>>data;
+            tail2->next = new Node(data);
+            tail2 = tail2->next;
+        }
+
+        Node *head = sortedMerge(head1, head2);
+        printList(head);
+    }
+    return 0;
+}
+
+Node* sortedMerge(Node* a, Node* b)  
+{  
+    // code here
+    Node *head=NULL,*tail=NULL;
+    if(a==NULL)      return b;
+    if(b==NULL)      return a;
+    if(a->data<b->data){
+        tail=a;
+        a=a->next;
+    }
+    else{
+        tail=b;
+        b=b->next;
+    }
+    head=tail;
+    while(a!=NULL && b!=NULL){
+        if(a->data<b->data){
+            tail->next=a;
+            tail=a;
+            a=a->next;
+        }
+        else{
+            tail->next=b;
+            tail=b;
+            b=b->next;
+        }
+    }
+    tail->next=(a==NULL)?b:a;
+    return head;
+}  
