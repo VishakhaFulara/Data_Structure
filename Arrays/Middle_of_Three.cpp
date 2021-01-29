ee673661d9501724d658f7c2b18961d368cd6254
VishakhaFulara <75986128+VishakhaFulara@users.noreply.github.com>
2021-01-29 23:09:15 +0530

    Create Middle_of_Three.cpp

---
+++ Arrays/Middle_of_Three.cpp
@@ -0,0 +1,56 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        if(A>B && B>C || C>B && B>A)
+            return B;
+        else if(B>A && A>C || C>A && A>B)
+            return A;
+        else
+            return C;
+            
+        /*if(A>B && A>C){
+            if(B>C)
+                return B;
+            else
+                return C;
+        }
+        else if(B>A && B>C){
+            if(A>C)
+                return A;
+            else
+                return C;
+        }
+        else if(A>B)
+                return A;
+            else
+                return B;
+        */
+            
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
