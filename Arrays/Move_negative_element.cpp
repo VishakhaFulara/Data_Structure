9431b1941a1b6ffb9519941f6c365e58e5ab0cd5
VishakhaFulara <75986128+VishakhaFulara@users.noreply.github.com>
2021-04-09 00:05:37 +0530

    Update Move_negative_element.cpp

--- Arrays/Move_negative_element.cpp
+++ Arrays/Move_negative_element.cpp
@@ -5,18 +5,24 @@ using namespace std;
 void separate(int a[],int n){
     int i=0,j=n-1;
     while(i<j){
+	 
+	 //if both are -ve numbers
         if(a[i]<0 && a[j]<0){
             i++;
             if(a[i]>0){
                 swap(a[i++],a[j--]);
             }
         }
+	    
+	//if both are positive numbers
         else if(a[i]>0 && a[j]>0){
             j--;
             if(a[j]<0){
                 swap(a[i++],a[j--]);
             }
         }
+	    
+	 //if one +ve & other is -ve
         else if(a[i]>0 &&  a[j]<0){
             swap(a[i++],a[j--]);
         }
