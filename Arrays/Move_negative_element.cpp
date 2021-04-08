316f95bd13b9f31fbe3d009a9dc5bd43bbbe9c40
VishakhaFulara <75986128+VishakhaFulara@users.noreply.github.com>
2021-04-08 23:47:46 +0530

    Create Move_negative_element.cpp

---
+++ Arrays/Move_negative_element.cpp
@@ -0,0 +1,41 @@
+//Move all the negative elements to one side of the array 
+#include <iostream>
+using namespace std;
+
+void separate(int a[],int n){
+    int i=0,j=n-1;
+    while(i<j){
+        if(a[i]<0 && a[j]<0){
+            i++;
+            if(a[i]>0){
+                swap(a[i++],a[j--]);
+            }
+        }
+        else if(a[i]>0 && a[j]>0){
+            j--;
+            if(a[j]<0){
+                swap(a[i++],a[j--]);
+            }
+        }
+        else if(a[i]>0 &&  a[j]<0){
+            swap(a[i++],a[j--]);
+        }
+        else{
+            i++;
+            j--;
+        }
+    }
+}
+int main() {
+    int n;
+    cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+	    cin>>arr[i];
+	}
+	separate(arr,n);
+	for(int i=0;i<n;i++){
+	    cout<<arr[i]<<" ";
+	}
+	return 0;
+}
